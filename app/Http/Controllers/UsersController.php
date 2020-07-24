604cc77e5367b8f14bd9c45290b0cf4ea2f28d3b
Kazuya Kojima <KT20173OCCUPy@gmail.com>
2020-07-24 22:15:26 +0900

    投稿機能とユーザーの投稿一覧画面まで

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,10 +4,17 @@ namespace App\Http\Controllers;
 
 use App\User;
 use Auth;
+use Validator;
 use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
+    public function __construct()
+    {
+        // ログインしていなかったらログインページに遷移する（この処理を消すとログインしなくてもページを表示する）
+        $this->middleware('auth');
+    }
+
     public function show($user_id)
     {
         $user = User::where('id', $user_id)->firstOrFail();
@@ -21,5 +28,31 @@ class UsersController extends Controller
 
          // テンプレート「user/edit.blade.php」を表示します。
         return view('user/edit', ['user' => $user]);
+    }
+
+    public function update(Request $request)
+    {
+        //バリデーション（入力値チェック）
+        $validator = Validator::make($request->all() , [
+            'user_name' => 'required|string|max:255',
+            'user_password' => 'required|string|min:6|confirmed',
+        ]);
+
+        //バリデーションの結果がエラーの場合
+        if ($validator->fails())
+        {
+          return redirect()->back()->withErrors($validator->errors())->withInput();
+        }
+
+        $user = User::find($request->id);
+        $user->name = $request->user_name;
+        if ($request->user_profile_photo !=null) {
+            $request->user_profile_photo->storeAs('public/user_images', $user->id . '.jpg');
+            $user->profile_photo = $user->id . '.jpg';
+        }
+        $user->password = bcrypt($request->user_password);
+        $user->save();
+
+        return redirect('/users/'.$request->id);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,10 @@ Route::get('/', 'PostsController@index');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+// Route::get('/home', 'HomeController@index')->name('home');
+
+// このコードを追加する
+Route::get('/home', 'PostsController@index');
 
 //ユーザ編集画面
 Route::get('/users/edit', 'UsersController@edit');
@@ -23,3 +26,9 @@ Route::get('/users/edit', 'UsersController@edit');
 Route::post('/users/update', 'UsersController@update');
 
 Route::get('/users/{user_id}', 'UsersController@show');
+
+// 投稿新規画面
+Route::get('/posts/new', 'PostsController@new')->name('new');
+
+// 投稿新規処理
+Route::post('/posts','PostsController@store');
