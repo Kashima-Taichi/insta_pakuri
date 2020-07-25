c74787458234e60dc9a002f9be9969b8c38b88b8
Kazuya Kojima <KT20173OCCUPy@gmail.com>
2020-07-25 16:12:36 +0900

    完成

--- app/Post.php
+++ app/Post.php
@@ -14,4 +14,15 @@ class Post extends Model
     public function likes() {
         return $this->hasMany('App\Like');
     }
+
+    public function likedBy($user)
+    {
+        return Like::where('user_id', $user->id)->where('post_id', $this->id);
+    }
+
+    //hasMany設定
+    public function comments()
+    {
+        return $this->hasMany('App\Comment');
+    }
 }
--- app/User.php
+++ app/User.php
@@ -34,4 +34,10 @@ class User extends Authenticatable
     public function likes() {
         return $this->hasMany('App\Like');
     }
+
+    //hasMany設定
+    public function comments()
+    {
+        return $this->hasMany('App\Comment');
+    }
 }
